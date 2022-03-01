f24d8e0b665fcc9a47e5cbb7111538875ea22471
flylee <flyleekungfu@163.com>
2022-03-01 16:19:00 +0800

    Gateway过滤器

---
+++ cloud-gateway-gateway9527/src/main/java/com/windyboy/springcloud/filter/GatewayFilter.java
@@ -0,0 +1,34 @@
+package com.windyboy.springcloud.filter;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("进入拦截器");
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StrUtil.isBlank(uname)) {
+            log.info("用户名为空");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
